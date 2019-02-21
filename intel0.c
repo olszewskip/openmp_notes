fbe50d0db27c5b46b787aaf3d8f19e9af920c141
olszewskip <olspaw@gmail.com>
2019-02-21 16:44:22 +0100

    Do hello world

---
+++ intel0.c
@@ -0,0 +1,12 @@
+// copyright: Tim Mattson (Intel)
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+#pragma omp parallel
+  {
+    int ID = 0;
+    printf("hello(%d) ", ID);
+    printf("world(%d)\n", ID);
+  }
+}
