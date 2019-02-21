9f48d4070958631223f9e9a16f9f64bc2d970226
olszewskip <olspaw@gmail.com>
2019-02-21 17:08:24 +0100

    first pi

---
+++ pi0.c
@@ -0,0 +1,13 @@
+// copyright: Tim Mattson (Intel)
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+  double A[1000];
+  omp_set_num_threads(2);
+#pragma omp parallel
+  {
+    int ID = omp_get_thread_num();
+    printf("hello world from thread (%d)\n", ID);
+  }
+}
