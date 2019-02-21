95b56866c825922a205c7ad43c2c243a6e7e5a9e
olszewskip <olspaw@gmail.com>
2019-02-21 17:07:48 +0100

    Rename helloworlds

--- hello2.c
+++ hello2.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 
 int main() {
+    double A[1000];
+    omp_set_num_threads(2);
 #pragma omp parallel
   {
     int ID = omp_get_thread_num();
-    printf("hello(%d) ", ID);
-    printf("world(%d)\n", ID);
+    printf("hello world from thread (%d)\n", ID);
   }
 }
