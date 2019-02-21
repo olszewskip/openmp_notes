b5a20f47b1ff8892497a2b1471f15c82b0635766
olszewskip <olspaw@gmail.com>
2019-02-21 20:25:43 +0100

    make a simple lock example

--- pi5.c
+++ pi5.c
@@ -2,33 +2,23 @@
 #include <omp.h>
 #include <stdio.h>
 
-#define NUM_THREADS 2
+#define NUM_THREADS 4
 
 static long num_steps = 100000000;
 int main() {
-  int nthreads = 0;
-
-  double pi = 0.0;
-
   const double step = 1.0 / (double)num_steps;
-
-  omp_set_num_threads(NUM_THREADS);
+  double pi = 0.0;
+  
   double time0 = omp_get_wtime();
-
+  omp_set_num_threads(NUM_THREADS);
   #pragma omp parallel
   {
-    int id = omp_get_thread_num();
-    int nthrds = omp_get_num_threads();
-    if (id == 0) nthreads = nthrds;
-
-    double x;
-    double local_sum = 0.0;
-    for (int i = id; i < num_steps; i += nthrds) {
-      x = (i + 0.5) * step;
-      local_sum += 4.0 / (1.0 + x * x);
-    }
-    #pragma omp atomic
-        pi += local_sum;
+  double x;
+  #pragma omp for reduction(+ : pi) schedule(static)
+  for (int i = 0; i < num_steps; i++) {
+    x = (i + 0.5) * step;
+    pi += 4.0 / (1.0 + x * x);
+  }
   }
 
   pi *= step;
